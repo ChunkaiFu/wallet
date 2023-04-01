7cf0ab157e4b3bf0492765b39202c02008fa11a9
Chunkai <chunkai369@gmail.com>
2023-04-01 11:48:10 -0500

    fixed the schema

--- app/models/tweet.rb
+++
@@ -1,4 +0,0 @@
-# class Tweet < ApplicationRecord
-#   belongs_to :user
-#   belongs_to :twitter_account
-# end
--- app/models/twitter_account.rb
+++
@@ -1,5 +0,0 @@
-# class TwitterAccount < ApplicationRecord
-#   belongs_to :user
-
-#   validates :username, uniqueness: true 
-# end
