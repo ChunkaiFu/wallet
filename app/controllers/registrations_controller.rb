4a3b61c041c9ca687b25cac1ed81b7363fe53384
Chunkai <chunkai369@gmail.com>
2023-04-15 02:05:20 -0500

    added validate email registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,10 @@ class RegistrationsController < ApplicationController
 
     def create 
         @user=User.new(user_params)
-        if @user.save 
+        if User.email_registered?(@user.email)
+            @user.errors.add(user_params[:email], " has already been registered")
+            render :new
+        elsif @user.save 
             session[:user_id] = @user.id
             redirect_to wallet_path, notice: "Successfully created account!"
         else 
@@ -19,3 +22,17 @@ class RegistrationsController < ApplicationController
         params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
     end 
 end
+
+
+def create
+    @user = User.new(user_params)
+    if User.email_registered?(@user.email)
+      @user.errors.add(:email, "has already been registered")
+      render :new
+    elsif @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
