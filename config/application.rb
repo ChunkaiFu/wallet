73677901f8163e26d96dd899fe4fa20d7b8efd25
Chunkai <chunkai369@gmail.com>
2023-04-29 00:21:24 -0500

    big to huge wallet name

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module WalletV3
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.action_mailer.default_url_options = { host: 'bigwallet.herokuapp.com' }
+    config.action_mailer.default_url_options = { host: 'hugewallet.herokuapp.com' }
     
   end
 end
