4fe6d729ed9945e661c89780a9164a33b23f74bc
Polkampally Keerthana <polkampallykeerthana@Polkampallys-MacBook-Pro.local>
2023-05-01 13:11:48 -0500

    done

--- app/admin/kyc.rb
+++ app/admin/kyc.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register Kyc do
         f.input :license_number, input_html: { value: f.object.license_number }, readonly: true
         end
         f.actions do
-          f.action :submit, label: "Update KYC", url: admin_dashboard_path, notice: "KYC status was successfully updated."
+          f.action :submit, label: "Update KYC Status", url: admin_dashboard_path, notice: "KYC status was successfully updated."
         end
     end
     def update
