cd15a4fb96480ee00ae5ab5094d986702da12abe
Chunkai <chunkai369@gmail.com>
2023-05-03 21:50:28 -0500

    change host address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: "virtualwallet-final.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "bigwallet.herokuapp.com" }
 
   def get_secret(secret_id)
     credentials = Aws::Credentials.new(
