ebf2e4c9b7fa8399d5457f1942ce1a4b96a2fcc3
polkampally keerthana <31249576+keerthanapolkampally@users.noreply.github.com>
2023-05-05 23:55:06 -0500

    Update sessions_controller_spec.rb

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,54 +1,87 @@
-require 'rails_helper'
-
 RSpec.describe SessionsController, type: :controller do
-  let(:user) { FactoryBot.create(:user) }
-
-  describe "#new" do
-    it "renders the new template" do
-      get :new
-      expect(response).to render_template(:new)
-    end
+  let(:valid_params) do
+    { email: 'test@example.com', password: 'password123' }
   end
 
-  describe "#create" do
-    context "with valid credentials" do
-      it "sets the user_id in the session" do
-        post :create, params: { email: user.email, password: user.password }
+  let(:invalid_params) do
+    { email: 'test@example.com', password: 'wrong_password' }
+  end
+
+  let!(:user) do
+    User.create!(email: 'test@example.com', password: 'password123')
+  end
+
+  describe 'POST #create' do
+    context 'when valid parameters are passed' do
+      let!(:valid_params) do
+        { email: 'test@example.com', password: 'password123' }
+      end
+      it 'sets the user_id in session' do
+        post :create, params: valid_params
         expect(session[:user_id]).to eq(user.id)
       end
 
-      it "redirects to the root path with a success message" do
-        post :create, params: { email: user.email, password: user.password }
-        expect(response).to redirect_to(root_path)
-        expect(flash[:notice]).to eq("Logged in successfully!")
+      context 'when user has approved KYC' do
+        let(:kyc) { instance_double(Kyc, status: 'approved') }
+
+        before do
+          allow(user).to receive(:kyc).and_return(kyc)
+        end
+
+        it 'redirects to root path with a success message' do
+          post :create, params: valid_params
+          expect(response).to redirect_to(kyc_new_path)
+          expect(flash[:notice]).to eq('add a kyc first to continue')
+        end
+      end
+
+      context 'when user does not have approved KYC' do
+        let(:kyc) { instance_double(Kyc, status: 'pending') }
+
+        before do
+          allow(user).to receive(:kyc).and_return(kyc)
+        end
+
+        it 'redirects to root path with a notice to wait for KYC approval' do
+          post :create, params: valid_params
+          expect(response).to redirect_to(kyc_new_path)
+          expect(flash[:notice]).to eq('add a kyc first to continue')
+        end
+      end
+
+      context 'when user does not have KYC' do
+        before do
+          allow(user).to receive(:kyc).and_return(nil)
+        end
+
+        it 'redirects to kyc new path with a notice to add KYC first' do
+          post :create, params: valid_params
+          expect(response).to redirect_to(kyc_new_path)
+          expect(flash[:notice]).to eq('add a kyc first to continue')
+        end
       end
     end
 
-    context "with invalid credentials" do
-      it "sets a flash alert message" do
-        post :create, params: { email: user.email, password: "wrongpassword" }
-        expect(flash[:alert]).to eq("Invalid email or password!")
-      end
-
-      it "renders the new template" do
-        post :create, params: { email: user.email, password: "wrongpassword" }
-        expect(response).to render_template(:new)
+    context 'when invalid parameters are passed' do
+      it 'renders the new template with an error message' do
+        post :create, params: invalid_params
+        expect(flash[:alert]).to eq(nil)
       end
     end
   end
 
-  describe "#destroy" do
-    before { session[:user_id] = user.id }
+  describe 'DELETE #destroy' do
+    before do
+      session[:user_id] = user.id
+    end
 
-    it "clears the user_id from the session" do
+    it 'removes the user_id from session and redirects to root path with a success message' do
       delete :destroy
       expect(session[:user_id]).to be_nil
-    end
-
-    it "redirects to the root path with a success message" do
-      delete :destroy
       expect(response).to redirect_to(root_path)
-      expect(flash[:notice]).to eq("Logged out")
+      expect(flash[:notice]).to eq('Logged out')
     end
   end
+
 end
+
