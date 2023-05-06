753425842f8f8edc3e5ff4d8a2ac026e6cf78e4e
polkampally keerthana <31249576+keerthanapolkampally@users.noreply.github.com>
2023-05-05 23:54:42 -0500

    Update registrations_controller_spec.rb

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,44 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
-    describe "GET #new" do
-      it "returns a successful response" do
-        get :new
-        expect(response).to be_successful
+  describe "GET #new" do
+    it "returns a successful response" do
+      get :new
+      expect(response).to be_successful
+    end
+
+    it "assigns a new User instance to @user" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid parameters" do
+      let(:valid_params) { { user: { email: "test@example.com", firstname: "John", lastname: "Doe", password: "password123", password_confirmation: "password123" } } }
+
+      it "creates a new user" do
+        expect { post :create, params: valid_params }.to change { User.count }.by(1)
       end
-  
-      it "assigns a new User instance to @user" do
-        get :new
-        expect(assigns(:user)).to be_a_new(User)
+
+      it "redirects to the root path with a success message" do
+        post :create, params: valid_params
+        expect(response).to redirect_to(kyc_new_path)
+        expect(flash[:notice]).to eq("Successfully created account!")
       end
     end
-  
-    describe "POST #create" do
-      context "with valid parameters" do
-        let(:valid_params) { { user: { email: "test@example.com", firstname: "John", lastname: "Doe", password: "password", password_confirmation: "password" } } }
-  
-        it "creates a new user" do
-          expect { post :create, params: valid_params }.to change(User, :count).by(1)
-        end
-  
-        it "redirects to the root path with a success message" do
-          post :create, params: valid_params
-          expect(response).to redirect_to(root_path)
-          expect(flash[:notice]).to eq("Successfully created account!")
-        end
+
+    context "with invalid parameters" do
+      let(:invalid_params) { { user: { email: "invalid_email", firstname: "John", lastname: "Doe", password: "password123", password_confirmation: "different_password" } } }
+
+      it "does not create a new user" do
+        expect { post :create, params: invalid_params }.not_to change { User.count }
       end
-  
-      context "with invalid parameters" do
-        let(:invalid_params) { { user: { email: "invalid_email", firstname: "John", lastname: "Doe", password: "password", password_confirmation: "different_password" } } }
-  
-        it "does not create a new user" do
-          expect { post :create, params: invalid_params }.not_to change(User, :count)
-        end
-  
-        it "renders the new template again" do
-          post :create, params: invalid_params
-          expect(response).to render_template(:new)
-        end
+
+      it "renders the new template again" do
+        post :create, params: invalid_params
+        expect(response).to render_template(:new)
       end
     end
-  end  
+  end
+end
